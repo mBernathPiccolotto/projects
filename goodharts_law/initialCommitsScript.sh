29f2b2ebb88af0e2fb6f7ac1d0581ea849c6397f
Marcelo Bernath Piccolotto <68627670+mBernathPiccolotto@users.noreply.github.com>
2025-03-17 22:55:49 -0400

    Update initialCommitsScript.sh

--- goodharts_law/initialCommitsScript.sh
+++ goodharts_law/initialCommitsScript.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+GIT_COMMITTER_NAME="mBernathPiccolotto"
+GIT_AUTHOR_NAME="mBernathPiccolotto"
 
-start_date="2023-01-01"  # Change this to your desired start date
-end_date=$(date -I)       # Sets end date to today's date
+start_date="2024-03-17"  # Change this to your desired start date
+end_date=$(date -I)       # Sets end date to today
 
 while [ "$start_date" != "$end_date" ]; do
   day_of_week=$(date -d "$start_date" +%u)  # 1 = Monday, 7 = Sunday
-  
+
   # Determine whether to skip the day
   if { [ $day_of_week -ge 6 ] && [ $((RANDOM % 10)) -lt 8 ]; } ||  # 80% skip on weekends
      { [ $day_of_week -lt 6 ] && [ $((RANDOM % 10)) -lt 2 ]; }; then  # 20% skip on weekdays
@@ -17,10 +18,13 @@ while [ "$start_date" != "$end_date" ]; do
     for ((i=0; i<num_commits; i++)); do
       echo "Commit $i on $start_date" > file.txt  # Modify a file
       git add file.txt
-      GIT_AUTHOR_DATE="$start_date 12:00:00" GIT_COMMITTER_DATE="$start_date 12:00:00" git commit -m "Backdated commit #$i for $start_date"
+      GIT_AUTHOR_NAME="$GIT_AUTHOR_NAME" GIT_AUTHOR_EMAIL="$GIT_AUTHOR_EMAIL" \
+      GIT_COMMITTER_NAME="$GIT_COMMITTER_NAME" GIT_COMMITTER_EMAIL="$GIT_COMMITTER_EMAIL" \
+      GIT_AUTHOR_DATE="$start_date 12:00:00" GIT_COMMITTER_DATE="$start_date 12:00:00" \
+      git commit -m "Backdated commit #$i for $start_date"
     done
   fi
-  
+
   # Increment the date
   start_date=$(date -I -d "$start_date + 1 day")
 done
